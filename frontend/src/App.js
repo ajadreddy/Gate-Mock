2ec584c13e0e132434abb3403b82994389e54499
ajadreddy <78726978+ajadreddy@users.noreply.github.com>
2022-03-14 04:41:20 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ frontend/src/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+import Home from './components/Home';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import {useEffect, useState } from 'react';
+import Cse from './components/gate/Cse';
+import Exam from './components/Exam';
+import Login from './components/login/Login';
+import Register from './components/register/Register';
+import Test1 from './components/test1/Test1';
+import Test4 from './components/test1/Test4';
+import Test5 from './components/test1/Test5';
+// import Test2 from './components/test1/Test2';
+// import Test3 from './components/test1/Test3';
+function App() {
+  const [ user, setLoginUser] = useState({})
+
+  useEffect(() => {
+    setLoginUser(JSON.parse(localStorage.getItem("MyUser")))
+  }, [])
+
+
+  const updateUser = (user) => {
+    if(user){
+      localStorage.setItem("MyUser", JSON.stringify(user))
+    setLoginUser(user)
+    }
+    
+  }
+
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path='/' >
+          {
+              user && user._id ? <Home updateUser={updateUser} /> : <Login updateUser={updateUser}/>
+            }
+          </Route>
+          <Route path='/cse'><Cse/></Route>
+          <Route path='/exam' ><Exam/></Route>
+          <Route path='/login' ><Login updateUser={updateUser}/></Route>
+          <Route path='/register' ><Register/></Route>
+          <Route path='/test1'> <Test1/> </Route>
+          <Route path='/test4'><Test4/></Route>
+          <Route path='/test5'><Test5/></Route>
+          {/*<Route path='/test2'><Test2/></Route>
+          <Route path='/test3'><Test3/></Route> */}
+        </Switch>
+      </Router>
+      
+    </>
+  );
+}
+  
+export default App;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Navbar = () => {
+
+  const logout =()=>{
+    localStorage.clear();
+    window.location.href = '/';
+  }
+  return <>
+      <div className='flex bg-blue-600 h-12'>
+        <div className='p-2 font-semibold text-white text-2xl'><a href="/">Mock Test</a></div>
+        <div className='p-2 '>
+          <button className='absolute right-2 w-20 px-2 py-1 text-base font-semibold text-white bg-sky-400 hover:bg-sky-500 rounded' onClick={logout}>Logout</button>
+        </div>
+    </div>
+  </>;
+};
+
+export default Navbar;
